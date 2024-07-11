d218127216501d0bf4df2a57ffb4c6f3c46538f4
Suya6021 <smangaj@jspmrscoe.edu.in>
2024-07-12 01:35:16 +0530

    fix:routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,24 +4,20 @@ import MemberLogin from "./pages/MemberLogin";
 import OrgLogin from "./pages/OrgLogin";
 import Home from "./pages/Home";
 
-
 const App = () => {
   return (
     <>
-    
       <Routes>
-        <Route path="/" Component={Home} />
-        <Route path="/joinus" Component={JoinUs} />
-            
-        
-        <Route path="/FootPrints" />
-        <Route path="/Resources" />
-        <Route path="/Events" />
-        <Route path="/ContextUs" />
-        <Route path="/MemberLogin" Component={MemberLogin}/>
-        <Route path="/OrgLogin" Component={OrgLogin}/>
+        <Route path="/" Component={Home}></Route>
+        <Route path="/join-us" Component={JoinUs}></Route>
+
+        <Route path="/FootPrints"></Route>
+        <Route path="/Resources"></Route>
+        <Route path="/Events"></Route>
+        <Route path="/ContextUs"></Route>
+        <Route path="/MemberLogin" Component={MemberLogin}></Route>
+        <Route path="/OrgLogin" Component={OrgLogin}></Route>
       </Routes>
-      
     </>
   );
 };
